197f5530180f5474afb1ca5096037a5c04dfd3e3
Baha Bilgin <bahablgn@gmail.com>
2020-11-13 01:53:26 -0500

    remote config setup

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -427,7 +427,7 @@ initFirebaseAuth();
 function remoteConfigInit(){
   const remoteConfig = firebase.remoteConfig();
   remoteConfig.settings = {
-    minimumFetchIntervalMillis: 5000,
+    minimumFetchIntervalMillis: 0,
   };
   
   remoteConfig.defaultConfig = ({
